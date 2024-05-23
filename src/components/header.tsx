5a0f6485febf3293f7734cf5a4eaad8739cd0cfe
Danilel <DanielOrtega8788@outlook.com>
2024-05-22 22:34:32 -0500

    started the repository

---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+ import React from "react";
+ 
+ const Header: React.FC = () => {
+return(
+<nav className=" flex mt-8 ml-4 justify-between">
+    <div className="flex">
+        <span className=" bg-gray-600 rounded-full w-10 mr-3">
+        </span>
+        <div className="flex items-baseline">
+            <h1 className=" font-serif font-extrabold text-4xl mr-3">Daniel Ortega</h1>
+            <h4 className=" font-small">Developer</h4>
+    </div>
+</div>
+    <div className="mr-4 flex">
+        <h1>
+            Contact
+            <span className=" ml-2 border-l-2 border-black"></span>
+        </h1>
+        <h1 className="ml-2 mr-2">
+            Resume
+            <span className=" ml-2 border-l-2 border-black"></span>
+        </h1>
+        <h1>
+            Project
+            <span className=" ml-2 border-l-2 border-black"></span>
+        </h1>
+    </div>
+</nav>
+)
+};
+export default Header
---
+++ src/components/hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero: React.FC = () => {
+    return (
+        <div>
+            hero page
+        </div>
+    )
+};
+
+export default Hero;

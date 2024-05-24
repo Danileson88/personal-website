9adccd24af5549fd0bd97986f74f1f4b6c73b266
Danilel <DanielOrtega8788@outlook.com>
2024-05-23 22:16:18 -0500

    worked on the navbar and hero of front page

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Hero: React.FC = () => {
-    return (
-        <div>
-            hero page
-        </div>
-    )
+  return (
+    <div className=" grid grid-cols-1 grid-rows-1 justify-items-center">
+      <div>
+        <img
+          src="./src/assets/profile-pic.JPG"
+          className=" w-[400px] h-[400px] border-solid rounded-full object-cover mt-32"
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Roboto', 'sans-serif'],
+          bebas: ['Bebas Neue']
+        },
+      },
+    },
   plugins: [],
 }
 

64e98f0de249005187e78202fcbac772f569d0d6
Danilel <DanielOrtega8788@outlook.com>
2024-06-04 22:06:57 -0500

    worked on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,30 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className=" mt-32 flex justify-center md:block">
+    <footer className=" mt-32 hidden md:flex justify-center lg:block">
       <ul
-        className="border-t-4 border-gray-400 border-solid absolute md:static bottom-[-500px] md:w-auto
+        className=" flex justify-between items-center h-32 border-t-4 border-gray-400 border-solid absolute md:static bottom-[-500px] md:w-auto
      w-11/12 md:mr-20 md:ml-20"
       >
-        <li>hello</li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <p className=" font-bold text-xl">Phone</p>
+          <a href="tel:8063191187">(806)-319-1187</a>
+        </li>
+        <li>
+          <p className=" font-bold text-xl">Email</p>
+          <a href="mailto:DanielOrtega8788@outlook.com">
+            DanielOrtega8788@outlook.com
+          </a>
+          <li />
+        </li>
+        <li>
+          <p className=" font-bold text-xl">Follow Me</p>
+          <a className="flex">
+            <img src="assets/facebook-icon.png" />
+            <img src="assets/indeed-icon.png" />
+          </a>
+        </li>
+        <li>&copy; 2024 By Daniel Ortega.</li>
       </ul>
     </footer>
   );
